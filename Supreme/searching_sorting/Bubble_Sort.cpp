1d463ad4f57501ea8ff5d1e49bef4c1dd00ed0a6
vikash kumar <vikashkashyap14955@gmail.com>
2024-10-14 15:52:58 +0530

    correction

--- Supreme/searching_sorting/Bubble_Sort.cpp
+++ Supreme/searching_sorting/Bubble_Sort.cpp
@@ -14,7 +14,7 @@ void BubbleSort(vector<int> &v){
     int n=v.size();
     for(int i=0;i<n-1;i++){
         for(int j=0;j<n-i-1;j++){
-            if(v[j] < v[j+1]){                   // increasing order ----(>)
+            if(v[j] > v[j+1]){                   // increasing order ----(>)
                 swap(v[j],v[j+1]);
             }
         }
